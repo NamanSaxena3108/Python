8f3224608105b7f92f48232e581b0de774656201
NamanSaxena3108 <166891809+NamanSaxena3108@users.noreply.github.com>
2024-04-13 09:49:38 +0530

    Add files via upload

---
+++ naman.py
@@ -0,0 +1,16 @@
+import array as arr
+arr=[]
+#for i in range(0,10):
+#    c=int(input("enter the element to be added:"))
+#   arr.insert(i,c)
+# print ("the elements are", arr)
+l=int(input("enter the length of array"))
+a=int(input("enter the index to be inserted"))
+for i in range(0,l):
+    if i==a:
+        b=int(input("enter the value to be inserted"))
+        arr.insert(a,b)
+    else:
+        v=int(input("enter the element to be added"))
+        arr.insert(i,v)
+print("the modified array is ",arr)
---
+++ naman1.py
@@ -0,0 +1,3 @@
+a=int(input("enter the decimal number"))
+b=bin(a)[2:]
+print(b)
