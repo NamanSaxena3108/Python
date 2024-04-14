294838ed19e13d9759de8b2af0bd27cfe4f2a8d7
NamanSaxena3108 <166891809+NamanSaxena3108@users.noreply.github.com>
2024-04-14 17:21:04 +0530

    Update naman.py

--- naman.py
+++ naman.py
@@ -1,9 +1,9 @@
 import array as arr
 arr=[]
 #for i in range(0,10):
-#    c=int(input("enter the element to be added:"))
-#   arr.insert(i,c)
-# print ("the elements are", arr)
+ c=int(input("enter the element to be added:"))
+  arr.insert(i,c)
+ print ("the elements are", arr)
 l=int(input("enter the length of array"))
 a=int(input("enter the index to be inserted"))
 for i in range(0,l):
