5474ee0ea2761b4943933f25f386653e3bb643a9
NAMAN SAXENA <166891809+NamanSaxena3108@users.noreply.github.com>
2024-04-21 15:27:19 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -21,8 +21,6 @@ operations = {
 num1 = int(input("What's the first number?: "))
 for symbol in operations:
   print(symbol)
-
-#Here we select "+"
 operation_symbol = input("Pick an operation: ") 
 num2 = int(input("What's the next number?: "))
 calculation_function = operations[operation_symbol]
@@ -30,21 +28,12 @@ first_answer = calculation_function(num1, num2)
 
 print(f"{num1} {operation_symbol} {num2} = {first_answer}")
 
-#Here we select "*" which overides the "+" we selected on line 26.
+
 operation_symbol = input("Pick an operation: ") 
 num3 = int(input("What's the next number?: "))
-
-#Here the calculation_function selected will be the multiply() function
 calculation_function = operations[operation_symbol] 
 
-#Here the code will be:
-#second_answer = multiply(multiply(num1, num2), num3)
 second_answer = calculation_function(calculation_function(num1, num2), num3)
-#second_answer = 2 * 3 * 3 = 18
-#To fix this bug we need to change the code on line 42 to:
 second_answer = calculation_function(first_answer, num3)
-#In the next lesson, we will delete all the code from line 34-48 so don't worry
-#It won't affect your final project.
-#But it's a good oportunity to practice debugging. 🐞
 
 print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
