ad523b6dc605564116f496f2b212c3c9b2aeb48b
NamanSaxena3108 <naman2004saxena@gmail.com>
2024-09-17 17:43:38 +0530

    done

--- projects/band-name-generator.py
+++
@@ -1,7 +0,0 @@
-print("welcome to the band name generator.")
-print("what city do you grew up in")
-city=input()
-print("What is you pet name")
-pet=input()
-band_name=city+" "+pet
-print("the band name coulde be:\n",band_name)
